8e20934b5d37e72ef4cadf0da0b952c65a711af7
sirhmvfx <sirhmvfx@gmail.com>
2023-04-27 19:46:39 +0100

    got it

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
 const carousel = document.querySelector(".review-container")
-const arrowBtn = document.querySelector(".review i")
+const arrowBtn = document.querySelectorAll(".review i")
+const firstCardWidth = carousel.querySelector(".review-cards").offsetWidth;
+
+arrowBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn === "left" ? -firstCardWidth : firstCardWidth;
+    })
+})
 
 
