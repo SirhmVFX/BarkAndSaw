1ba97af42c5f0c27f62da9c500a4e1b9956ff14c
sirhmvfx <sirhmvfx@gmail.com>
2023-04-28 12:13:03 +0100

    oti sisie

--- public/script.js
+++ public/script.js
@@ -2,10 +2,8 @@ const carousel = document.querySelector(".review-container")
 const arrowBtn = document.querySelectorAll(".review button")
 const firstCardWidth = carousel.querySelector(".review-cards").offsetWidth;
 
-arrowBtn.forEach(btn => {
+arrowBtn.forEach( function(btn) { 
     btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn === "left" ? - firstCardWidth : firstCardWidth;
+        carousel.scrollLeft += btn.id === "left" ? - firstCardWidth : + firstCardWidth;
     })
 })
-
-
