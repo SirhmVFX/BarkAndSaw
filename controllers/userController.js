360435a0e55c562b454dcbbb5900a882f48caa31
sirhmvfx <sirhmvfx@gmail.com>
2023-05-02 00:27:49 +0100

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
 exports.homepage = function(req, res) {
     res.render("homepage", {pageTitle: "Bark&Saw"})
 }
+
+exports.login = function(req, res) {
+    res.render("login", {pageTitle: "Login-Bark&Saw"})
+}
--- router.js
+++ router.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const userController = require("./controllers/userController")
 
 router.get("/", userController.homepage)
+router.get("/login", userController.login)
 
 module.exports = router
