122a9f47217554b673ee8c55d82f5b8b3169feee
sirhmvfx <sirhmvfx@gmail.com>
2023-05-03 18:52:28 +0100

    Writing authentication logics

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const usersCollection = require("../db").db().collection("users")
 const bcrypt = require("bcryptjs")
+const validator = require("validator")
+const { default: isEmail } = require("validator/lib/isEmail")
 
 const User = function(data) {
     this.data = data
@@ -12,6 +14,8 @@ User.prototype.cleanUp = function() {
     if (typeof(this.data.email) != "string") {this.data.email = ""}
     if (typeof(this.data.password) != "string") {this.data.password = ""}
     if (typeof(this.data.confirmPassword) != "string") {this.data.confirmPassword = ""}
+
+    if (this.data.password !== this.data.confirmPassword) {this.errors.push("Passwords does not match")}
 
     this.data = {
         firstname: this.data.firstname.trim().toLowercase(),
@@ -29,7 +33,12 @@ User.prototype.validate = function() {
     if (this.data.firstname.length > 20 ) {this.errors.push("firstname can not me more than 20 characters")}
     if (this.data.lastname.length > 20 ) {this.errors.push("lastname can not me more than 20 characters")}
 
-    if () {}
+    if (!isAlphanumeric(this.data.firstname)) {this.errors.push("firstname should consists of character only")}
+    if (!isAlphanumeric(this.data.lastname)) {this.errors.push("lastname should consists of character only")}
+
+    if (!isEmail(this.data.email)) {this.errors.push("please enter a valid email address")}
+
+    if (this.data.password.length < 8 && this.data.password.length > 20) {this.errors.push("Password must be atleast 8 characters and not more than 20 characters")}
 }
 
 User.prototype.register = function(){
