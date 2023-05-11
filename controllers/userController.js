f1b6a4615a958a35af32078aec41ec1ed88a9d9e
sirhmvfx <sirhmvfx@gmail.com>
2023-05-11 01:19:55 +0100

    style element

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,7 @@ exports.register = function(req, res) {
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result){
-        req.session.user = {email: user.data.email, _id: user.data._id}
+        req.session.user = {email: user.data.email, _id: user.data._id, name: user.data.firstname}
         req.session.save(function() {
             res.redirect("/") 
         })
